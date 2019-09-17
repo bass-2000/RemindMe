047c82515bfe7d6672b0c6f9ba7b5abe74eeda27
bass-2000 <bass-2000@yandex.ru>
2019-09-17 19:45:29 +0300

    Added Toolbar and search icon

--- app/src/main/java/com/bass2000/remindme/MainActivity.java
+++ app/src/main/java/com/bass2000/remindme/MainActivity.java
@@ -1,15 +1,34 @@
 package com.bass2000.remindme;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
