2311a20a34b5fe9caeec7b949dd19e502cafd89a
bass-2000 <bass-2000@yandex.ru>
2019-09-17 20:39:21 +0300

    Added NavigationView

--- app/src/main/java/com/bass2000/remindme/MainActivity.java
+++ app/src/main/java/com/bass2000/remindme/MainActivity.java
@@ -1,19 +1,20 @@
 package com.bass2000.remindme;
 
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
