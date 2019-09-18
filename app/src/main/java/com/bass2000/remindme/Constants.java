868d60dd9f9d60006ea3824e913952814b81a273
bass-2000 <bass-2000@yandex.ru>
2019-09-18 22:06:06 +0300

    fix Notification icon in nav.menu works correct

--- app/src/main/java/com/bass2000/remindme/Constants.java
+++ app/src/main/java/com/bass2000/remindme/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final int TAB_ONE = 0;
     public static final int TAB_TWO = 1;
     public static final int TAB_THREE = 2;
+    public static final int TAB_FOUR = 2;
 
 }
